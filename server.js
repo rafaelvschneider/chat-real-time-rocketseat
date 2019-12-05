45d025ffa4e67c9095e58aacc63d6bcdfbf8a7e2
Rafael Schneider <rafaelvschneider@gmail.com>
2019-12-04 21:32:47 -0300

    iniciando desenvolvimento

--- server.js
+++ server.js
@@ -18,8 +18,12 @@ let messages = [];
 
 io.on('connection', socket => {
     console.log(`Socket conectado ${socket.id}`);
+
+    socket.emit('previousMessages', messages);
+
     socket.on('sendMessage', data => {
         messages.push(data);
+        socket.broadcast.emit('receivedMessage', data);
     });
 });
 
